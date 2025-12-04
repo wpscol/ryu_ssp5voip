36f7c25ada570d9e61b5e81be8d86c137f7c8205
wpscol <witoldp.school@protonmail.com>
2025-12-04 01:27:08 +0100

    update: traffic generator + flow installation on stp controller

--- mininet/ryu_project.py
+++ mininet/ryu_project.py
@@ -1,39 +1,85 @@
-#
-# SIMPLE L2 EXAMPLE
-# Flood packets to other interfaces
-#
-
-from ryu.base import app_manager
+from ryu.app import simple_switch_stp_13
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.lib.packet import packet, ethernet, ipv4, tcp, udp
+from ryu.lib import stplib
 
-from logging import getLogger, INFO
 
-log = getLogger("ryu")
-log.setLevel(INFO)
-
-class L2Switch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
-
+class STP_Switch(simple_switch_stp_13.SimpleSwitch13):
     def __init__(self, *args, **kwargs):
-        super(L2Switch, self).__init__(*args, **kwargs)
+        super(STP_Switch, self).__init__(*args, **kwargs)
 
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    # We must override the handler to add Layer 4 (Port) matching logic
+    @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        ofp_parser = dp.ofproto_parser
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match["in_port"]
 
-        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dst = eth.dst
+        src = eth.src
 
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # Learn the MAC address
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.mac_to_port[dpid][src] = in_port
+
+        # Determine output port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # --- MODIFICATION START: Install Flow with L4 Matching ---
+        if out_port != ofproto.OFPP_FLOOD:
+
+            # 1. Base Match (Layer 2)
+            match_fields = {"in_port": in_port, "eth_dst": dst, "eth_src": src}
+
+            # 2. Add Layer 3 (IP) and Layer 4 (TCP/UDP) specific matches
+            ip_pkt = pkt.get_protocol(ipv4.ipv4)
+            if ip_pkt:
+                match_fields["eth_type"] = 0x0800  # IPv4
+                match_fields["ipv4_src"] = ip_pkt.src
+                match_fields["ipv4_dst"] = ip_pkt.dst
+                match_fields["ip_proto"] = ip_pkt.proto
+
+                # Check for TCP
+                tcp_pkt = pkt.get_protocol(tcp.tcp)
+                if tcp_pkt:
+                    match_fields["tcp_src"] = tcp_pkt.src_port
+                    match_fields["tcp_dst"] = tcp_pkt.dst_port
+
+                # Check for UDP
+                udp_pkt = pkt.get_protocol(udp.udp)
+                if udp_pkt:
+                    match_fields["udp_src"] = udp_pkt.src_port
+                    match_fields["udp_dst"] = udp_pkt.dst_port
+
+            # 3. Create the Match object and add flow
+            match = parser.OFPMatch(**match_fields)
+            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            return
+        # --- MODIFICATION END ---
+
+        # If we didn't install a flow (flood), simply send the packet out
         data = None
-        if msg.buffer_id == ofp.OFP_NO_BUFFER:
-             data = msg.data
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
 
-        out = ofp_parser.OFPPacketOut(
-            datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data = data)
-        dp.send_msg(out)
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(out)
--- mininet/topology.py
+++ mininet/topology.py
@@ -7,58 +7,84 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.node import RemoteController
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
 log = getLogger("py3mininet")
 
 
 class SSP5VoipTopology(Topo):
-  def build(self):
-    h1 = self.addHost("h1")
-    h2 = self.addHost("h2")
+    def build(self):
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
 
-    # Switches
-    s1 = self.addSwitch('s1')
-    s2 = self.addSwitch('s2')
-    s3 = self.addSwitch('s3')
-    s4 = self.addSwitch('s4')
-    s5 = self.addSwitch('s5')
-    s6 = self.addSwitch('s6')
+        # Switches
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+        s4 = self.addSwitch("s4")
+        s5 = self.addSwitch("s5")
+        s6 = self.addSwitch("s6")
 
-    # Links
-    self.addLink(h1, s1)
-    self.addLink(s1, s3)
-    self.addLink(s1, s4)
-    self.addLink(s1, s5)
-    self.addLink(s5, s6)
-    self.addLink(s3, s2)
-    self.addLink(s4, s2)
-    self.addLink(s6, s2)
-    self.addLink(s2, h2)
+        # Links
+        self.addLink(h1, s1)
+        self.addLink(s1, s3)
+        self.addLink(s1, s4)
+        self.addLink(s1, s5)
+        self.addLink(s5, s6)
+        self.addLink(s3, s2)
+        self.addLink(s4, s2)
+        self.addLink(s6, s2)
+        self.addLink(s2, h2)
+
+
+def configure_tc(net):
+    for link in net.links:
+        for intf in (link.intf1, link.intf2):
+            intf.config(bw=10.0, delay="2ms")
+
+
+def start_traffic(net):
+    """Starts permanent traffic generation between hosts."""
+    h1 = net.get("h1")
+    h2 = net.get("h2")
+
+    # Based on your logs, h2 (10.0.0.2) is the Server, h1 is the Client
+    srv_ip = h2.IP()
+
+    info(f"*** Starting iperf SERVERS on {h2.name} ({srv_ip})...\n")
+    h2.cmd("iperf -s -p 5001 &")
+    h2.cmd("iperf -s -p 5002 &")
+    h2.cmd("iperf -s -u -p 5003 &")  # UDP Server
+
+    info(f"*** Starting iperf CLIENTS on {h1.name} connecting to {h2.name}...\n")
+    # -t 99999 ensures they run effectively forever during the lab
+    h1.cmd(f"iperf -c {srv_ip} -p 5001 -t 99999 &")
+    h1.cmd(f"iperf -c {srv_ip} -p 5002 -t 99999 &")
+    h1.cmd(f"iperf -c {srv_ip} -p 5003 -u -t 99999 &")
 
 
 def main():
-  topo = SSP5VoipTopology()
+    topo = SSP5VoipTopology()
+    net = Mininet(
+        topo=topo, controller=None, autoSetMacs=True, autoStaticArp=True, link=TCLink
+    )
 
-  net = Mininet(topo=topo, controller=None, autoSetMacs=True, autoStaticArp=True, link=TCLink)
-  net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
-  net.start()
+    # Note: Ensure a controller (like Ryu) is running on port 6633 locally
+    net.addController("c0", controller=RemoteController, ip="127.0.0.1", port=6633)
 
-  # remove loops (STP)
-  for sw in net.switches:
-    sw.cmd('ovs-vsctl set-fail-mode', sw, 'standalone')
-    sw.cmd('ovs-vsctl set Bridge', sw, 'stp_enable=true')
+    net.start()
+    configure_tc(net)
 
-  # Configure TC
-  for link in net.links:
-    for intf in (link.intf1, link.intf2):
-      intf.config(bw=5, delay='15ms')
+    info("\n*** Waiting 45 seconds for STP to converge...\n")
+    sleep(45)
 
-  CLI(net)
-  net.stop()
+    start_traffic(net)
+
+    CLI(net)
+    net.stop()
 
 
 if __name__ == "__main__":
-  setLogLevel("info")
-  main()
+    setLogLevel("info")
+    main()
